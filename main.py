ffecf5d5fb4ccd897942dd6fabfe3d4d92db40f6
Gontarenok <vika030395@gmail.com>
2023-06-28 04:36:52 +0300

    That's all :)

--- main.py
+++ main.py
@@ -1,53 +1,48 @@
 from time import sleep
-import re
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import chromedriver_binary
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Chrome()
 
-browser.maximize_window()
-browser.get('https://online.metro-cc.ru/category/bytovaya-himiya/chistyaschie-sredstva?in_stock=1&order=price_asc')
+def get_page_html(url):
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get(url=url)
+    sleep(3)
 
-modal_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[1]'
-WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, modal_xpath)))
-browser.find_element(By.XPATH, modal_xpath).click()
+    # закрываем всплывающее окно
+    modal_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[1]'
+    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, modal_xpath)))
+    browser.find_element(By.XPATH, modal_xpath).click()
 
-moskow_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[1]'
-WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, moskow_xpath)))
-browser.find_element(By.XPATH, modal_xpath).click()
+    # подтверждаем выбор города Москва
+    moscow_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[1]'
+    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, moscow_xpath)))
+    browser.find_element(By.XPATH, modal_xpath).click()
 
+    # для выбора Санкт-Петербурга
+    # choice_city_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[2]'
+    # saint_petersburg_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/div/span[2]'
 
-# count_xpath = '//*[@id="catalog-wrapper"]/main/div[2]/div[1]/span'
-# WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, count_xpath)))
-# count = browser.find_element(By.XPATH, count_xpath)
-#
-# count_int = re.sub(r'\D', '', count.text)
-# print(f'Количество товаров в категории: {count_int}')
+    # Кнопка "показать еще", нажимаем пока находим ее на странице
+    more_xpath = '//*[@id="catalog-wrapper"]/main/div[2]/button'
+    while True:
+        try:
+            if browser.find_element(By.XPATH, more_xpath):
+                browser.find_element(By.XPATH, more_xpath).click()
+                sleep(2)
+        except:
+            with open('result_pars_2.html', 'w') as file:
+                file.write(browser.page_source)
+            break
 
-
-# Кнопка "показать еще", нажимаем пока находим ее на странице
-more_xpath = '//*[@id="catalog-wrapper"]/main/div[2]/button'
-
-while True:
-    try:
-        if browser.find_element(By.XPATH, more_xpath):
-            browser.find_element(By.XPATH, more_xpath).click()
-            sleep(2)
-    except:
-        with open('result_pars.html', 'w') as file:
-            file.write(browser.page_source)
-        break
-
-    # WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, more_xpath)))
+    browser.close()
 
 
-sleep(10)
+def main():
+    get_page_html('https://online.metro-cc.ru/category/bytovaya-himiya/chistyaschie-sredstva?in_stock=1&order=price_asc')
 
-browser.close()
 
-choice_city_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/button[2]'
-saint_peterburg_xpath = '//*[@id="__layout"]/div/div/div[7]/div[2]/div[2]/div/span[2]'
+if __name__ == "__main__":
+    main()
--- parse.py
+++ parse.py
@@ -1,65 +1,85 @@
 import json
-
+import requests
 from bs4 import BeautifulSoup
 
-def get_urls(file_path):
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+}
+
+def get_products(file_path):
     with open(file_path) as file:
         src = file.read()
 
         soup = BeautifulSoup(src, 'html.parser')
         products = soup.find_all('div', class_='product-card')
-        print(len(products))
+
         urls = []
         product_list = []
+
         for product in products:
-            # product_img = item.find('div', class_='product-card-photo__content').find('img').get('src')
+            # Получаем все нужные данные для списка product_list
             product_id = product.get('data-sku')
             product_name = product.find('div', class_='product-card-photo__content').find('a').get('title')
             product_url = 'https://online.metro-cc.ru' + product.find('div', class_='product-card-photo__content').find('a').get('href')
+            urls.append(product_url)   # сохраняем ссылки также в отдельном списке, для выгрузки в отдельный файл
 
             # Достаем актуальную цену из span-ов с рублями и копейками, если все найдено, склеиваем в итоговую цену
-            regular_price_rubles = product.find('span', class_='product-card-prices__actual').find('span', class_='product-price__sum-rubles')
-            regular_price_penny = product.find('span', class_='product-card-prices__actual').find('span', class_='product-price__sum-penny')
+            regular_price_rubles = product.find('span', class_='product-card-prices__actual').find('span',
+                                                                                                   class_='product-price__sum-rubles')
+            regular_price_penny = product.find('span', class_='product-card-prices__actual').find('span',
+                                                                                                  class_='product-price__sum-penny')
             if regular_price_rubles is not None:
-                regular_price = regular_price_rubles.text
+                regular_price = regular_price_rubles.text.strip()
                 if regular_price_penny is not None:
-                    regular_price += regular_price_penny.text
+                    regular_price += regular_price_penny.text.strip()
             else:
                 regular_price = None
 
             # Для акционных товаров указана старая цена, находим ее если указана
             promo_price_rubles = product.find('span', class_='product-card-prices__old')
             if promo_price_rubles is not None:
-                promo_price = promo_price_rubles.find('span', class_='product-price__sum-rubles').text
-                promo_price_penny = product.find('span', class_='product-card-prices__old').find('span', class_='product-price__sum-penny')
+                promo_price = promo_price_rubles.find('span', class_='product-price__sum-rubles').text.strip()
+                promo_price_penny = product.find('span', class_='product-card-prices__old').find('span',
+                                                                                                 class_='product-price__sum-penny')
                 if promo_price_penny is not None:
-                    promo_price += promo_price_penny.text
+                    promo_price += promo_price_penny.text.strip()
             else:
                 promo_price = None
 
-            # brand =
 
-            # item_url = item.find('a', class_='product-card-name'.find('span')).text
-            product_list.append(promo_price)
+            # Для брендов заходим на страницу товара
+            response = requests.get(url=product_url, headers=headers)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            try:
+                brand = soup.find('div', class_='product-page-content__attributes-full').find('span', class_='product-attributes__list-item-value').text.strip()
+            except Exception as _ex:
+                brand = None
 
+            # Собираем все данные по товару в список
+            product_list.append({
+                "id": product_id,
+                "name": product_name,
+                "url": product_url,
+                "regular_price": regular_price,
+                "promo_price": promo_price,
+                "brand": brand
+            })
 
-            # product_list.append({
-            #     "id": product_id,
-            #     "name": product_name,
-            #     "url": product_url,
-            #     "regular_price": regular_price,
-            #     "promo_price": promo_price,
-            #     "brand": brand
-            # })
-        print(product_list)
         print(len(product_list))
 
         # with open('items_urls.txt', 'w') as file:
         #     for url in urls:
         #         file.write(f'{url}\n')
 
-        # with open('products.json', 'w') as file:
-        #     json.dump(product_list, file, indent=4)
+        with open('products.json', 'w', encoding='utf-8') as file:
+            json.dump(product_list, file, indent=4, ensure_ascii=False)
 
 
-get_urls(file_path='result_pars.html')
+
+def main():
+    get_products(file_path='result_pars.html')
+
+
+if __name__ == "__main__":
+    main()
